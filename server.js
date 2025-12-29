3aef588b59b744e183744ce227def3bdc345608a
dineshkumartlbupi <dineshkumartlbupi@gmail.com>
2025-12-29 22:40:33 +0530

    Sync server and env templates

--- server.js
+++ server.js
@@ -14,15 +14,9 @@ console.log('-----------------------');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware
-// app.use(cors({
-//     origin: '*', // Allow all origins for testing
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['Content-Type']
-// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Configure multer for file uploads (in memory)
 const storage = multer.memoryStorage();
 const upload = multer({
