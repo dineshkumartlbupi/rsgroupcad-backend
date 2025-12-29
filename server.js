450b0583b67b0b930a7ecc3132e6b53d5983c8df
dineshkumartlbupi <dineshkumartlbupi@gmail.com>
2025-12-29 22:42:21 +0530

    fix issues

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ console.log('-----------------------');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+// test
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
